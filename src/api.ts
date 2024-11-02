41ac1c65a7f7bb7f2016219280e1a115155a480c
skyu93 <seungkyu0215@gmail.com>
2024-11-02 16:08:50 +0900

    feat: #6, Dummy API 연동

---
+++ src/api.ts
@@ -0,0 +1,53 @@
+import { PaginatedProductsResponse } from '@/types/product';
+import { API_ENDPOINTS } from '@/config/api';
+
+export const fetchProducts = async (
+	props?: Partial<{
+		limit: number;
+		page: number;
+	}>
+): Promise<PaginatedProductsResponse | null> => {
+	try {
+		const { limit = 20, page = 0 } = props ?? {};
+		const res = await fetch(
+			`${API_ENDPOINTS.PRODUCT}?limit=${limit}&skip=${page}`
+		);
+		return res.json();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
+export const fetchProductById = async (productId: string) => {
+	try {
+		const res = await fetch(`${API_ENDPOINTS.PRODUCT}?${productId}`);
+		return res.json();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
+
+export const fetchProductCategories = async (): Promise<string[] | null> => {
+	try {
+		const res = await fetch(`${API_ENDPOINTS.PRODUCT}/categories'`);
+		return res.json();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
+
+export const fetchSearchProduct = async (
+	queryString: string
+): Promise<string | null> => {
+	try {
+		const res = await fetch(
+			`${API_ENDPOINTS.PRODUCT}/search?q=${queryString}'`
+		);
+		return res.json();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
